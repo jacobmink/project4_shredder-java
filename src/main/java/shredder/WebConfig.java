2c3d2fd6146592eb977f09e915197709052d644e
Jacob Mink <jacobmink@gmail.com>
2019-02-28 16:26:27 -0700

    deploy plz

--- src/main/java/shredder/WebConfig.java
+++ src/main/java/shredder/WebConfig.java
@@ -15,7 +15,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "${REACT_FRONTEND}")
+                .allowedOrigins("**")
+//                .allowedOrigins("http://localhost:3000", "${REACT_FRONTEND}")
                 .allowedMethods("PUT", "DELETE", "POST", "OPTIONS", "HEAD", "GET")
                 .allowedHeaders("*")
                 .allowCredentials(true);
