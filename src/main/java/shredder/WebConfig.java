cb54b4d34e8802e64c86a4e1a6697a8619d002c0
Jacob Mink <jacobmink@gmail.com>
2019-03-01 11:24:20 -0700

    deploy plz

--- src/main/java/shredder/WebConfig.java
+++ src/main/java/shredder/WebConfig.java
@@ -1,22 +1,33 @@
 package shredder;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.ArrayList;
+
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
+//    @Value("${ALLOWED_ORIGINS}")
+//    private String frontEnd;
+
+    @Autowired
+    private Environment env;
+
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("*")
-//                .allowedOrigins("http://localhost:3000", "${REACT_FRONTEND}")
+                .allowedOrigins("http://localhost:3000", "${ALLOWED_ORIGINS}")
+//                .allowedOrigins(env.getProperty("ALLOWED_ORIGINS"))
                 .allowedMethods("PUT", "DELETE", "POST", "OPTIONS", "HEAD", "GET")
                 .allowedHeaders("*")
                 .allowCredentials(true);
