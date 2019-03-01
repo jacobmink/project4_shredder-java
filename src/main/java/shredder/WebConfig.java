8f4a2f50d3993baeb79e63392555bf4c5607f81a
Jacob Mink <jacobmink@gmail.com>
2019-03-01 11:48:54 -0700

    deploy

--- src/main/java/shredder/WebConfig.java
+++ src/main/java/shredder/WebConfig.java
@@ -26,7 +26,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins(env.getProperty("ALLOWED_ORIGINS"))
+                .allowedOrigins("https://shredder-react.herokuapp.com/")
 //                .allowedOrigins(env.getProperty("ALLOWED_ORIGINS"))
                 .allowedMethods("PUT", "DELETE", "POST", "OPTIONS", "HEAD", "GET")
                 .allowedHeaders("*")
